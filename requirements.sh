1dede303963c83ca584989438d0c97a4d87cf8b3
Le Huynh Duc <lhduc94@gmail.com>
2018-08-10 01:18:14 +0700

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
 
-#DATADIR=/datasets/
-#mkdir -p $DATADIR
-wget -O iris.csv http://s3.amazonaws.com/assets.datacamp.com/staging/course_2406/datasets/iris.csv
+DATADIR=/datasets/
+mkdir -p $DATADIR
+wget -O $DATADIR/members.csv https://assets.datacamp.com/production/repositories/3365/datasets/0dfd9cf73134137fc9e7abcbdce4a3d8af269ae9/members.csv
 
 # If bash command fails, build should error out
 set -e
